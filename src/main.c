6310ca8e68a0d915e9ca79ed41227745d878a2fe
skakavyponik <j.bobosik@gmail.com>
2016-10-11 16:15:43 +0200

    funkcia_blikanie

--- src/main.c
+++ src/main.c
@@ -46,48 +46,83 @@ SOFTWARE.
 **
 **===========================================================================
 */
+void ADC_init()
+	{
+		GPIO_InitTypeDef GPIO_InitStructure;
+		 ADC_InitTypeDef ADC_InitStructure;
+		/* Enable GPIO clock */
+			 RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
+			 /* Configure ADCx Channel 2 as analog input */
+			 GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 ;
+			 GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AN;
+			 GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL ;
+			 GPIO_Init(GPIOA, &GPIO_InitStructure);
+			/* Enable the HSI oscillator */
+			 RCC_HSICmd(ENABLE);
+			/* Check that HSI oscillator is ready */
+			 while(RCC_GetFlagStatus(RCC_FLAG_HSIRDY) == RESET);
+			 /* Enable ADC clock */
+			 RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
+			 /* Initialize ADC structure */
+			 ADC_StructInit(&ADC_InitStructure);
+			 /* ADC1 configuration */
+			 ADC_InitStructure.ADC_Resolution = ADC_Resolution_12b;
+			 ADC_InitStructure.ADC_ContinuousConvMode = DISABLE;
+			 ADC_InitStructure.ADC_ExternalTrigConvEdge = ADC_ExternalTrigConvEdge_None;
+			 ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
+			 ADC_InitStructure.ADC_NbrOfConversion = 1;
+			 ADC_Init(ADC1, &ADC_InitStructure);
+			/* ADCx regular channel8 configuration */
+			 ADC_RegularChannelConfig(ADC1, ADC_Channel_0, 1, ADC_SampleTime_16Cycles);
+			 /* Enable the ADC */
+			 ADC_Cmd(ADC1, ENABLE);
+			 /* Wait until the ADC1 is ready */
+			 while(ADC_GetFlagStatus(ADC1, ADC_FLAG_ADONS) == RESET)
+			 	 {
+			 	 }
+			 	 /* Start ADC Software Conversion */
+			 	 ADC_SoftwareStartConv(ADC1);
+
+	}
+
+void Dio_init()
+	{
+		RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
+		/*nastavenie diody */
+		GPIO_InitTypeDef gpioInitStruc;
+
+		gpioInitStruc.GPIO_Mode = GPIO_Mode_OUT;
+		gpioInitStruc.GPIO_OType = GPIO_OType_PP;
+		gpioInitStruc.GPIO_Pin = GPIO_Pin_5;
+		gpioInitStruc.GPIO_PuPd= GPIO_PuPd_UP;
+		gpioInitStruc.GPIO_Speed = GPIO_Speed_40MHz;
+
+		GPIO_Init(GPIOA, &gpioInitStruc);
+		GPIO_SetBits(GPIOA, GPIO_Pin_5);
+	}
+
+void blikanie(int cas)
+{
+	int i=0;
+	for (i = 0; i < cas; i++)
+		  	  	  	  {
+		  	  	  	  }
+		  GPIO_ToggleBits(GPIOA, GPIO_Pin_5);
+}
 int main(void)
 {
-	GPIO_InitTypeDef GPIO_InitStructure;
-	 ADC_InitTypeDef ADC_InitStructure;
-	 /* Enable GPIO clock */
-	 RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
-	 /* Configure ADCx Channel 2 as analog input */
-	 GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 ;
-	 GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AN;
-	 GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL ;
-	 GPIO_Init(GPIOA, &GPIO_InitStructure);
-	/* Enable the HSI oscillator */
-	 RCC_HSICmd(ENABLE);
-	/* Check that HSI oscillator is ready */
-	 while(RCC_GetFlagStatus(RCC_FLAG_HSIRDY) == RESET);
-	 /* Enable ADC clock */
-	 RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
-	 /* Initialize ADC structure */
-	 ADC_StructInit(&ADC_InitStructure);
-	 /* ADC1 configuration */
-	 ADC_InitStructure.ADC_Resolution = ADC_Resolution_12b;
-	 ADC_InitStructure.ADC_ContinuousConvMode = DISABLE;
-	 ADC_InitStructure.ADC_ExternalTrigConvEdge = ADC_ExternalTrigConvEdge_None;
-	 ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
-	 ADC_InitStructure.ADC_NbrOfConversion = 1;
-	 ADC_Init(ADC1, &ADC_InitStructure);
-	/* ADCx regular channel8 configuration */
-	 ADC_RegularChannelConfig(ADC1, ADC_Channel_0, 1, ADC_SampleTime_16Cycles);
-	 /* Enable the ADC */
-	 ADC_Cmd(ADC1, ENABLE);
-	 /* Wait until the ADC1 is ready */
-	 while(ADC_GetFlagStatus(ADC1, ADC_FLAG_ADONS) == RESET)
-	 {
-	 }
-	 /* Start ADC Software Conversion */
-	 ADC_SoftwareStartConv(ADC1);
-	 double AD_value=0;
+	double AD_value=0;
+
+
+	ADC_init();
+	Dio_init();
+
   while (1)
   {
 	  ADC_SoftwareStartConv(ADC1);
 	  while(!ADC_GetFlagStatus(ADC1, ADC_FLAG_EOC)){}
 	  AD_value=ADC_GetConversionValue(ADC1);
+	  blikanie(1000000);
   }
   return 0;
 }
